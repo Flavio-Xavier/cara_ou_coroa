8435e20cdec0c446dcbc1aa0b113a0cb54b56078
Flavio-Xavier <flaviojuniorx@yahoo.com.br>
2022-05-12 16:03:02 -0300

    Criado o app Cara ou coroa utilizando o Flutter e a linguagem dart.

---
+++ lib/Jogar.dart
@@ -0,0 +1,43 @@
+import 'package:cara_ou_coroa/Resultado.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Jogar extends StatefulWidget {
+  
+  @override
+  State<Jogar> createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+
+  void _exibirResultado(){
+    var itens = ["cara", "coroa"];
+    var numero = Random().nextInt(itens.length);
+    var resultado = itens[numero];
+    Navigator.push(
+      context, 
+      MaterialPageRoute(
+        builder: (context) => Resultado(resultado)
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset("imagens/logo.png"),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("imagens/botao_jogar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Resultado.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatefulWidget {
+
+  String valor;
+
+  Resultado(this.valor);
+  
+  @override
+  State<Resultado> createState() => _ResultadoState();
+}
+
+class _ResultadoState extends State<Resultado> {
+
+  @override
+  Widget build(BuildContext context) {
+  var caminhoImagem;
+  if(widget.valor == "cara"){
+    caminhoImagem = "imagens/moeda_cara.png";
+  }else{
+    caminhoImagem = "imagens/moeda_coroa.png";
+  }
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset(caminhoImagem),
+            GestureDetector(
+              onTap: (){
+                Navigator.pop(context);
+              },
+              child: Image.asset("imagens/botao_voltar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
